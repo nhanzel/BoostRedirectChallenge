d1a6cd75d03efc943ea072aa7164843e4ad7656b
Nathan Hanzel <Nhanzel@DHICorp.com>
2022-04-07 14:57:34 -0600

    Formatting the redirect object in our router class

--- BoostRedirectChallenge/Router.cs
+++ BoostRedirectChallenge/Router.cs
@@ -10,12 +10,24 @@ namespace BoostRedirectChallenge
     {
         public IEnumerable<string> Process(IEnumerable<string> routes)
         {
+            List<RedirectPath> paths = new List<RedirectPath>();
 
+            List<KeyValuePair<string, string>> redirects = new List<KeyValuePair<string, string>>();
+            foreach (var route in routes)
+            {
+                var routePieces = route.Split("->");
+                paths.Add(new RedirectPath(routePieces.ToList()));
+            }
+            return new List<string>();
         }
     }
 
     public class RedirectPath
     {
+        public RedirectPath(List<string> _path)
+        {
+            Path = _path;
+        }
         public List<string> Path { get; set; } = new List<string>();
     }
 }
